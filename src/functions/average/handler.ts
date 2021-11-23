181e728da91016fa124849f083db80bf2a9cbadb
Hipólito Gomez Cuello <hipolitogomezcuello@gmail.com>
2021-11-22 22:46:18 -0300

    Added cache system for all endpoints

--- src/functions/average/handler.ts
+++ src/functions/average/handler.ts
@@ -2,11 +2,26 @@ import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
 import { formatJSONResponse } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
 import blueUSDService from 'src/services/blueUSDService';
+import dbService from 'src/services/dbService';
 
 const average: ValidatedEventAPIGatewayProxyEvent<void> = async () => {
+  let cache;
+  try {
+    cache = await dbService.findByKey("average-cache", "quotes-info");
+  } catch(err) {
+    console.log(err);
+  }
+  if (!!cache && cache.lastUpdated > (new Date().getTime() - 60000)) {
+    return formatJSONResponse(cache.response);
+  }
   const allQuotes = await blueUSDService.getAllQuotes();
   const averageQuote = blueUSDService.getAverage(allQuotes);
+  await dbService.save("average-cache", "quotes-info", {
+    lastUpdated: new Date().getTime(),
+    response: averageQuote,
+  });
   return formatJSONResponse(averageQuote);
+
 }
 
 export const main = middyfy(average);
--- src/functions/slippage/handler.ts
+++ src/functions/slippage/handler.ts
@@ -2,9 +2,23 @@ import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
 import { formatJSONResponse } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
 import blueUSDService from 'src/services/blueUSDService';
+import dbService from 'src/services/dbService';
 
 const slippages: ValidatedEventAPIGatewayProxyEvent<void> = async () => {
+  let cache;
+  try {
+    cache = await dbService.findByKey("slippage-cache", "quotes-info");
+  } catch(err) {
+    console.log(err);
+  }
+  if (!!cache && cache.lastUpdated > (new Date().getTime() - 60000)) {
+    return formatJSONResponse(cache.response);
+  }
   const slippages = await blueUSDService.getAllSlippages();
+  await dbService.save("slippage-cache", "quotes-info", {
+    lastUpdated: new Date().getTime(),
+    response: slippages,
+  });
   return formatJSONResponse(slippages);
 }
 
